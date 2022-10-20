6dd92e105475fb2b1cda13a74e861c7c6948313e
AlexisAraujo530 <alexisaraujo530@gmail.com>
2022-10-20 14:25:54 -0300

    Homework-3

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,22 @@
-var fs  = require("fs")
-var http  = require("http")
+const fs  = require("fs");
+const http  = require("http");
 
 // Escribí acá tu servidor
+http
+    .createServer((req, res) => {
+        const name = req.url.slice(1);
+        console.log(name);
+        const files = fs.readdirSync("./images");
+
+        for(const file of files){
+            if(file.includes(name)){
+                res.writeHead(200, {"Content-type": "image/jpg"});
+                const img = fs.readFileSync(`./images/${name}_doge.jpg`);
+                res.end(img);
+            
+        }
+    }
+    
+}).listen(3001, "localhost");
+
+
