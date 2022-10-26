088b31ea10c108a2aaaaed195156c759d08514f7
AlexisAraujo530 <alexisaraujo530@gmail.com>
2022-10-26 15:23:07 -0300

    Homework-6

--- 06-Testing/homework/index.js
+++ 06-Testing/homework/index.js
@@ -3,16 +3,47 @@ const app = express();
 
 app.use(express.json()); // for parsing application/json
 
+const sumArray = (array, num) => {
+  for(const num1 of array) {
+    for(const num2 of array) {
+      if(num1 === num2) break;
+      if(num1 + num2 === num) return true;
+    }
+  }
+  return false;
+}
+
+
 app.get('/', (req, res) => {
-  res.send({
+  res.status(200).send({
     message: 'hola',
   });
 });
 
-app.post('/product', (req, res) => {
-  res.send({
-    result: req.body.a / req.body.b,
+app.get('/test', (req, res) => {
+  res.status(200).send({
+    message: 'test',
+  });
+}); 
+
+app.post('/sum', (req, res) => {
+  res.status(200).send({
+    result: req.body.a + req.body.b,
   });
 });
+
+
+app.post('/product', (req, res) => {
+  res.status(200).send({
+    result: req.body.a * req.body.b,
+  });
+});
+
+app.post('/sumArray', (req, res) => {
+  const { array, num } = req.body;
+  const result = sumArray(array, num);
+  res.status(200).send({result});
+});
+
 
 module.exports = app; // Exportamos app para que supertest session la pueda ejecutar
--- 06-Testing/homework/tests/index.spec.js
+++ 06-Testing/homework/tests/index.spec.js
@@ -16,7 +16,7 @@ describe('Test de APIS', () => {
     it('responds with 200', () => agent.get('/test').expect(200));
     it('responds with and object with message `test`', () =>
       agent.get('/test').then((res) => {
-        expect(res.body.message).toEqual('hola');
+        expect(res.body.message).toEqual('test');
       }));
   });
 
@@ -43,14 +43,33 @@ describe('Test de APIS', () => {
   });
 
   describe('POST /sumArray', () => {
-    it('responds with 200', () => agent.get('/test').expect(200));
-    it('responds with and object with message `test`', () =>
-      agent.post('/sumArray')
+    it('responds with 200', () => agent
+    .post('/sumArray')
+    .send({array: [2,5,7,10,11,15,20], num: 13})
+    .expect(200));
+    it('responds with an object with the result true if combination of two numbers get the target', () =>
+      agent
+        .post('/sumArray')
         .send({array: [2,5,7,10,11,15,20], num: 13})
         .then((res) => {
           expect(res.body.result).toEqual(true);
       }));
+    it('responds with an object with the result false if combination of two numbers do not get the target', () =>{
+      return agent
+        .post('/sumArray')
+        .send({array: [1, 2, 3, 4, 5], num: 100})
+        .then((res) => {
+          expect(res.body.result).toEqual(false);
+      }
+    )});
+    it('no puede sumar dos numeros iguales', () =>{
+      return agent
+        .post('/sumArray')
+        .send({array: [1, 2, 3, 4], num: 2})
+        .then((res) => {
+          expect(res.body.result).toEqual(false);
+      })
+    });
   });
-
 });
 
